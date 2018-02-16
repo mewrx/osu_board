afe61aa39d6355285de364e2d07947cd5631b037
mewrx <mikola.vintonyak@gmail.com>
2018-02-16 15:04:19 +0200

    Update Keyboard_1.ino

--- Keyboard_1.ino
+++ Keyboard_1.ino
@@ -1,34 +1,34 @@
 #include "DigiKeyboard.h"
 
-typedef struct
-{
-byte Pin;
-byte Key;
-boolean Press;
-}BUTTON;
+typedef struct {
+  byte Pin;
+  byte Key;
+  boolean Press;
+} BUTTON;
 
-void ButtonRead(BUTTON* Btn)
-{
-int State = digitalRead(Btn->Pin);
-if(State==HIGH&&State!=Btn->Press)
-  {
-  Btn->Press = State;
-  DigiKeyboard.sendKeyPress(Btn->Key);
+void ButtonRead(BUTTON* Btn){
+  int State = digitalRead(Btn->Pin);
+  if(State==HIGH&&State!=Btn->Press){
+    Btn->Press = State;
+    DigiKeyboard.sendKeyPress(0);
   }
-else if(State==LOW&&State!=Btn->Press)
-  {
-  Btn->Press = State;
-BUTTON Btn2 = {1,KEY_X,false};
-void setup()
-{
-pinMode(0,INPUT_PULLUP);
-pinMode(1,INPUT_PULLUP);
-DigiKeyboard.sendKeyStroke(0);
+  else if(State==LOW&&State!=Btn->Press){
+    Btn->Press = State;
+    DigiKeyboard.sendKeyPress(Btn->Key);
+  }
 }
 
-void loop()
-{
-ButtonRead(&Btn1);
-ButtonRead(&Btn2);
-DigiKeyboard.delay(20);
+BUTTON Btn1 = {2,KEY_Z,false};
+BUTTON Btn2 = {0,KEY_X,false};
+
+void setup() {
+  pinMode(0,INPUT_PULLUP);
+  pinMode(2,INPUT_PULLUP);
+  DigiKeyboard.sendKeyStroke(0);
+}
+
+void loop() {
+  ButtonRead(&Btn1);
+  ButtonRead(&Btn2);
+  DigiKeyboard.delay(10);
 }
